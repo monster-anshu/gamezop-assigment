64c2e94a9019f11b9a8f84cf635c22cf741a2622
Himanshu Gunwant <hgunwant2312@gmail.com>
2024-11-26 18:02:02 +0530

    feat: add hindi in translation

--- apps/web/public/service-worker.js
+++ apps/web/public/service-worker.js
@@ -139,8 +139,9 @@ async function dynamicCaching(request) {
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
+  event.respondWith(dynamicCaching(request));
+  return;
   const url = new URL(request.url);
-
   if (url.pathname.startsWith("/api")) {
     event.respondWith(networkFirstStrategy(request));
   } else if (event.request.mode === "navigate") {
